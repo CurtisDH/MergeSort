01322c2a0291a1a13cd99eb1753c43bef7bc525f
CurtisDH <curtisdh0@gmail.com>
2023-09-08 19:31:06 +1000

    - averaging results for graph generation. Increased test sizw

--- GraphGenerator/main.py
+++ GraphGenerator/main.py
@@ -5,43 +5,41 @@ import numpy as np
 # Read CSV with column names
 df = pd.read_csv('performance_data.csv', names=['ArraySize', 'CoreCount', 'Time'])
 
+# Group the data by ArraySize and CoreCount and calculate the mean
+grouped = df.groupby(['ArraySize', 'CoreCount']).mean().reset_index()
+
 # Sort unique core counts
-sorted_core_counts = sorted(df['CoreCount'].unique())
+sorted_core_counts = sorted(grouped['CoreCount'].unique())
 
 # Create a larger figure
 plt.figure(figsize=(15, 10))
-
-# Initialize a flag to track if the single-core line has been plotted
-single_core_plotted = False
-
-# Loop through sorted unique core counts and plot data
-for core_count in sorted_core_counts:
-    subset = df[df['CoreCount'] == core_count]
-    # Check if this is the single-core line and if it has already been plotted
-    if core_count == 1 and single_core_plotted:
-        continue
-    plt.plot(subset['ArraySize'], subset['Time'], label=f'{core_count} Cores')
-    if core_count == 1:
-        single_core_plotted = True
 
 # Constants to scale the example curves
 scale_o_n = 0.0001
 scale_o_n_log_n = 0.00001
 
 # Generate example data for O(N), O(N log N)
-n_values = np.linspace(1, df['ArraySize'].max(), 500)
+n_values = np.linspace(1, grouped['ArraySize'].max(), 500)
 o_n = n_values * scale_o_n
 o_n_log_n = n_values * np.log(n_values) * scale_o_n_log_n
 
 plt.plot(n_values, o_n, label='O(N)', linestyle='--')
 plt.plot(n_values, o_n_log_n, label='O(N log N)', linestyle='--')
 
+# Loop through sorted unique core counts and plot averaged data
+for core_count in sorted_core_counts:
+    subset = grouped[grouped['CoreCount'] == core_count]
+    if core_count == 1:
+        plt.plot(subset['ArraySize'], subset['Time'], label=f'{core_count} Core')
+    else:
+        plt.plot(subset['ArraySize'], subset['Time'], label=f'{core_count} Cores')
+
 plt.xlabel('Array Size')
-plt.ylabel('Time (ms)')
+plt.ylabel('Average Time (ms)')
 plt.legend(loc='upper left')  # Place the legend in the upper left corner
 
 # Set x-axis ticks to display actual numbers
-xticks = np.linspace(0, df['ArraySize'].max(), 11)
+xticks = np.linspace(0, grouped['ArraySize'].max(), 11)
 xticklabels = [str(int(x)) for x in xticks]
 plt.xticks(xticks, labels=xticklabels, rotation=45)  # Rotate for better visibility
 
